ce90ec53be4800f0a0eeda8d15d369cc5b20bd6e
396266318@qq.com <396266318@qq.com>
2019-06-16 22:05:03 +0800

    登录
    登录

---
+++ test_platform/test_platform/urls.py
@@ -0,0 +1,36 @@
+"""test_platform URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.views.generic.base import RedirectView
+from user_app import views
+# from js_demo import js_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url='static/favicon.ico')),
+    # 用户应用
+    path('hello/', views.say_hello),
+    path('', views.index),
+    path('index/', views.index),
+    path('accounts/login/', views.index),
+    path('logout/', views.logout),
+    path('home/', views.home),
+    
+    
+    # 项目管理
+    # path('project/', views.),
+]
---
+++ test_platform/user_app/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+
+
+# MTV view
+def say_hello(request):
+	input_name = request.GET.get("name", "")
+	if input_name == "":
+		return HttpResponse("请求输入?name=name")
+	return render(request, 'index.html', {"name": input_name})
+
+
+# 登录首页
+def index(request):
+	if request.method == "GET":
+		return render(request, 'index.html')
+	else:
+		username = request.POST.get("username", "")
+		password = request.POST.get("password", "")
+		
+		if username == "" or password == "":
+			return render(request, "index.html", {"error": "用户名或密码为空"})
+		
+		user = auth.authenticate(username=username, password=password)
+		if user is None:
+			return render(request, "index.html", {"error": "用户名或密码错误"})
+		else:
+			auth.login(request, user)  # 记录用户的登录状态
+			# return HttpResponseRedirect("/project/")
+			return HttpResponseRedirect("/home/")
+
+
+# 处理用户的退出
+@login_required
+def logout(request):
+	auth.logout(request)
+	return HttpResponseRedirect("index.html")
+
+
+def home(request):
+	return render(request, 'home.html')
